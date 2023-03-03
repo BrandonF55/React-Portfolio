fdd6d38b8e0e18de6e84c05fda80cc7177b69888
Brandonf55 <brandon.fenk99@gmail.com>
2023-03-03 15:34:41 -0800

    added information to both GalleryGram and SillyStringFactory

--- src/components/ProjectPage/GalleryGram.jsx
+++ src/components/ProjectPage/GalleryGram.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import styles from './ProjectPage.module.scss';
 
-function GalleryGram(){
-    return(
-        <div>
+function GalleryGram() {
+    return (
+        <div className={styles.ProjectsContainer}>
             <h1><u>Welcome To The Gallery Gram</u></h1>
 
             <div className={styles.ProjectLinks}>
                 <a href="https://github.com/BrandonF55/GalleryGram.git" title="View Code" alt="View Code">
-                <h2>View Code Here</h2>
+                    <h2>View Code Here</h2>
                 </a>
-            </div> //end of block of code
+            </div> 
 
+            <h3>Technologies Used</h3>
             <div className={styles.IconContainer}>
                 <p className={styles.Icons}>C#</p>
                 <p className={styles.Icons}>ASP.NET</p>
@@ -21,9 +22,26 @@ function GalleryGram(){
                 <p className={styles.Icons}>CSS</p>
                 <p className={styles.Icons}>HTML</p>
             </div>
+
+
+                <h3>Description</h3>
             <em>
-            This is a Gallery application that utilizes uploading of pictures, liking pictures, and purchasing prints of the pictures that you enjoy. Utilizing a shopping cart API, authentication and authorization, and databases.Each user will create their own profile and gain access to their own profile showing photos that they have uploaded as well as the images that they have 'liked'.
+                This is a Gallery application that utilizes uploading of pictures, liking pictures, and purchasing prints of the pictures that you enjoy. Utilizing a shopping cart API, authentication and authorization, and databases.Each user will create their own profile and gain access to their own profile showing photos that they have uploaded as well as the images that they have 'liked'.
             </em>
+
+            <h3>Getting started with the GalleryGram</h3>
+            <li>
+                Users can create a account on the main page where it will create you a profile where you can upload, like, and print pictures you like on the application.
+            </li>
+            <h3>Interactive Application</h3>
+            <li>
+                On the Main Page of the application you'll see all of the pictures you liked awhile scrolling through and liking pictures. You can Fully order the picture of your choosing through a sand-box Api. It doesn't actually place the order but you get a order number once you hit the check out button.
+            </li>
+
+            <div>
+                <img src="" alt="" />
+            </div>
+
 
 
 
