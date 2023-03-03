612a34809e184dec1cd30a34e84231aeb4a15643
Brandonf55 <brandon.fenk99@gmail.com>
2023-03-03 11:34:27 -0800

    having problems getting a arrow to appear on my page

--- src/components/ProjectPage/ProjectPage.jsx
+++ src/components/ProjectPage/ProjectPage.jsx
@@ -3,12 +3,14 @@ import { Outlet } from "react-router-dom";
 import ProjectList from "./ProjectList";
 import styles from './ProjectPage.module.scss';
 import ProjectPageNavigation from'./ProjectPageNavigation'
+// import Arrow from "./Arrow";
 
 function ProjectPage(){
   return(
     <div className={styles.ProjectPage}>
       {/* <Outlet/> */}
       <ProjectPageNavigation/>
+      {/* <Arrow/> */}
       {/* <ProjectList/> */}
     </div>
   );
--- src/components/ProjectPage/ProjectPageNavigation.jsx
+++ src/components/ProjectPage/ProjectPageNavigation.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './ProjectPage.module.scss';
+import Arrow from './Arrow';
 
 function ProjectNavigation() {
   return (
   
     <nav className={styles.ProjectList}>
+ 
  
       <Link to="sillystringfactor" alt="SillyStringFactor" title="StillyStringFactor">
         <h1><u>Silly String Factory</u></h1>
@@ -15,9 +17,8 @@ function ProjectNavigation() {
       <Link to="gallerygram" alt="GalleryGram" title="GalleryGram">
         <h1><u>Gallery Gram</u></h1>
         <h4></h4>
-      
       </Link>
-    
+    <Arrow/>
     </nav>
   
 
