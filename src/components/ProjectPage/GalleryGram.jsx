1d0833ac0f412bc64997000e11b620cc550456f2
Brandonf55 <brandon.fenk99@gmail.com>
2023-03-03 18:50:43 -0800

    finished the Gallery Gram project page and now i need to style it

--- src/components/ProjectPage/GalleryGram.jsx
+++ src/components/ProjectPage/GalleryGram.jsx
@@ -2,40 +2,39 @@ import React from "react";
 import styles from './ProjectPage.module.scss';
 import ASP_Net from '../../assets/ASP_NET.png'
 import C from '../../assets/C#.png'
-import CSS1 from '../../assets/CSS1.png'
 import HTML from '../../assets/HTML.png'
 import MySQL from '../../assets/MySQL.png'
+import CSS from '../../assets/CSS.png'
+import Net from '../../assets/Net.png'
+import Postman from '../../assets/Postman.png'
 
 
 
 function GalleryGram() {
     return (
         <div className={styles.ProjectsContainer}>
+            
             <h1><u>Welcome To The Gallery Gram</u></h1>
 
             <div className={styles.ProjectLinks}>
                 <a href="https://github.com/BrandonF55/GalleryGram.git" title="View Code" alt="View Code">
                     <h2>View Code Here</h2>
                 </a>
-            </div> 
-
-            <h3>Technologies Used</h3>
-            <div className={styles.IconContainer}>
-
-                {/* <p className={styles.Icons}>{C}</p> */}
-                <img src={C} alt="" />
-                <p className={styles.Icons}>ASP.NET</p>
-                <p className={styles.Icons}>MySQL</p>
-                <p className={styles.Icons}>Entity Framework Core 6.0.0</p>
-                <p className={styles.Icons}>Postman</p>
-                <img src={CSS1} alt="" />
-                {/* <p className={styles.Icons}>{CSS1}</p> */}
-                <p className={styles.Icons}>HTML</p>
-            
             </div>
 
+            <h3><u>Technologies Used</u></h3>
 
-                <h3>Description</h3>
+            <div className={styles.IconContainer}>
+                <img src={C} alt="C#" />
+                <img src={ASP_Net} alt="ASP.Net" />
+                <img src={MySQL} alt="MySQL" />
+                <img src={Net} alt=".Net" />
+                <img src={CSS} alt="CSS" />
+                <img src={Postman} alt="Postman" />
+                <img src={HTML} alt="HTML" />
+            </div>
+
+            <h3><u>Description</u></h3>
             <em>
                 This is a Gallery application that utilizes uploading of pictures, liking pictures, and purchasing prints of the pictures that you enjoy. Utilizing a shopping cart API, authentication and authorization, and databases.Each user will create their own profile and gain access to their own profile showing photos that they have uploaded as well as the images that they have 'liked'.
             </em>
