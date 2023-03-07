43895aa47bfb4a53c88a9c622264125434fe0e2b
Brandonf55 <brandon.fenk99@gmail.com>
2023-03-07 10:38:48 -0800

    fininshed with all of the placement for everything in the website. added a live site to the mrFenks snowboards. I couldn't figure out how to deplay the the GalleryGram im not sure if it had something to do with the set up or it being a dotnet application. not sure.

--- src/components/ProjectPage/MrFenksSnowboards.jsx
+++ src/components/ProjectPage/MrFenksSnowboards.jsx
@@ -8,7 +8,7 @@ import React1 from '../../assets/React1.png'
 function MrFenksSnowboards() {
     return (
         <div className={styles.ProjectsContainer}>
-    <hr />
+            <hr />
             <h1><u>Welcome To Mr. Fenk's Snowboards</u></h1>
 
             <div>
@@ -16,7 +16,11 @@ function MrFenksSnowboards() {
 
                 <div className={styles.ProjectLinks}>
                     <a href="https://github.com/BrandonF55/MrFenks-Snowboards.git" title="View Code" alt="View Code">
-                        <h2>View Code Here</h2>
+                        <h2>View Code</h2>
+                    </a>
+
+                    <a href='https://storied-biscochitos-eeaa9f.netlify.app' title="View live Site" alt="View live Site">
+                        <h2> View Live Site</h2>
                     </a>
                 </div>
 
@@ -26,28 +30,29 @@ function MrFenksSnowboards() {
                     <img src={CSS} alt="CSS" />
                     <img src={HTML} alt="HTML" />
                     <img src={JS} alt="JS" />
-                    <img src={React1} alt="REACT" />
+                    <img src={React1} alt="REACT" className={`${styles.React1} ${styles.spin}`} />
+
                 </div>
-                <div className={styles.TextContainer}> 
-                <h3><u>Description</u></h3>
-                <em>
-                    This is a Small React Application For Mr Fenk's Snowboards. You can add a new snowboard, with terrain, price. You can Delete, Add, Edit, and sell a snowboard from the inventory
-                </em>
+                <div className={styles.TextContainer}>
+                    <h3><u>Description</u></h3>
+                    <em>
+                        This is a Small React Application For Mr Fenk's Snowboards. You can add a new snowboard, with terrain, price. You can Delete, Add, Edit, and sell a snowboard from the inventory
+                    </em>
 
 
-                <h3><u>Creating This Application</u></h3>
-                <li>
-                    With this architecture, the application is divided into three main components: the Model, which represents the data and logic of the application; the View, which represents the user interface; and the Controller, which acts as the intermediary between the Model and View. By leveraging React, I was able to create a powerful and dynamic user interface that was both easy to update and highly customizable. Throughout the project, I focused on planning and designing a clear and organized structure for the application, carefully breaking down the requirements and ensuring the components interacted seamlessly with each other. My attention to detail and persistence allowed me to build a successful MVC React application that met my goals and surpassed my expectations. I am proud of the skills I developed during this project and look forward to applying them to future endeavors.
-                </li>
+                    <h3><u>Creating This Application</u></h3>
+                    <li>
+                        With this architecture, the application is divided into three main components: the Model, which represents the data and logic of the application; the View, which represents the user interface; and the Controller, which acts as the intermediary between the Model and View. By leveraging React, I was able to create a powerful and dynamic user interface that was both easy to update and highly customizable. Throughout the project, I focused on planning and designing a clear and organized structure for the application, carefully breaking down the requirements and ensuring the components interacted seamlessly with each other. My attention to detail and persistence allowed me to build a successful MVC React application that met my goals and surpassed my expectations. I am proud of the skills I developed during this project and look forward to applying them to future endeavors.
+                    </li>
 
 
 
                 </div>
             </div>
 
-            
+
         </div>
-         
+
     )
 }
 
