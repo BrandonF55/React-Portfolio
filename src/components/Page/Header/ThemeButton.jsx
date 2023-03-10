d7d12e7c723ea0f3a0cd7494e3c06a95c686608c
Brandonf55 <brandon.fenk99@gmail.com>
2023-03-09 17:31:38 -0800

    commented out the themebutton

--- src/components/Page/Header/ThemeButton.jsx
+++ src/components/Page/Header/ThemeButton.jsx
@@ -1,31 +1,31 @@
-import { useThemeContext } from '../../../state/ThemeProvider';
-import styles from './ThemeButton.module.scss'
+// import { useThemeContext } from '../../../state/ThemeProvider';
+// import styles from './ThemeButton.module.scss'
 
 
 
-function ThemeButton() {
-    const { theme, setTheme } = useThemeContext();
+// function ThemeButton() {
+//     const { theme, setTheme } = useThemeContext();
   
-    const toggleTheme = () => {
-      if (theme === 'dark') {
-        setTheme('light');
-      } else {
-        setTheme('dark');
-      }
-      console.log(theme)
-    };
+//     const toggleTheme = () => {
+//       if (theme === 'dark') {
+//         setTheme('light');
+//       } else {
+//         setTheme('dark');
+//       }
+//       console.log(theme)
+//     };
   
-    const buttonClass = theme === 'dark' ? styles.dark : styles.light;
+//     const buttonClass = theme === 'dark' ? styles.dark : styles.light;
   
-    return (
-      <button
-        className={`${styles.button} ${buttonClass}`}
-        onClick={toggleTheme}
-      >
-        {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
-      </button>
-    );
-  }
+//     return (
+//       <button
+//         className={`${styles.button} ${buttonClass}`}
+//         onClick={toggleTheme}
+//       >
+//         {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+//       </button>
+//     );
+//   }
 
 
-export default ThemeButton;
+// export default ThemeButton;
--- src/components/ProjectPage/GalleryGram.jsx
+++ src/components/ProjectPage/GalleryGram.jsx
@@ -35,7 +35,7 @@ function GalleryGram() {
                 <img src={HTML} alt="HTML" />
             </div>
 
-            <div className={`${styles.TextContainer} ${styles.FadeIn}`}> // this is how you concatenate styling.
+            <div className={`${styles.TextContainer} ${styles.FadeIn}`}> 
                 <h3><u>Description</u></h3>
                 <em>
                     This is a Gallery application that utilizes uploading of pictures, liking pictures, and purchasing prints of the pictures that you enjoy. Utilizing a shopping cart API, authentication and authorization, and databases.Each user will create their own profile and gain access to their own profile showing photos that they have uploaded as well as the images that they have 'liked'.
