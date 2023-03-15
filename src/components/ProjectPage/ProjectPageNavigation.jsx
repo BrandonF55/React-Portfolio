9be03c5d49aec17cf5500e484da5571a5065186f
Brandonf55 <brandon.fenk99@gmail.com>
2023-03-15 09:28:53 -0700

    deleted h1 tags in ProjectPageNavigation

--- src/components/ProjectPage/ProjectPageNavigation.jsx
+++ src/components/ProjectPage/ProjectPageNavigation.jsx
@@ -13,12 +13,12 @@ function ProjectNavigation() {
  
       <Link to="mrfenks-snowboards" alt="MrFenks-Snowboards" title="MrFenks-Snowboards">
         <h1 className="animate__animated animate__backInLeft" ><u>MrFenks-Snowboards</u></h1>
-        <h4></h4>
+        
       </Link>
 
       <Link to="gallerygram" alt="GalleryGram" title="GalleryGram">
         <h1 className="animate__animated animate__backInRight" ><u>Gallery Gram</u></h1>
-        <h4></h4>
+
       </Link>
       <img src={Arrow_Image} alt="Arrow pointing up" className={styles.arrowImage} /> 
     </nav>
